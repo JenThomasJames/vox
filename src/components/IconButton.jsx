4d0623f057f23548c08840ebcb5e8dffe12d3e55
JenThomasJames <jenthomasjames123@gmail.com>
2023-05-08 23:01:02 +0530

    Fix: Add support for the IconButton component to render different sizes * A size prop passed from the parent can now handle the size of the IconButton

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-const IconButton = ({ icon }) => {
-  return (
-    <button className="p-2 rounded-full bg-slate-100 hover:bg-slate-200">
-      {icon}
-    </button>
-  );
+const IconButton = ({ icon, size = "small" }) => {
+  let styles = "rounded-full bg-slate-100 hover:bg-slate-200 ";
+  switch (size) {
+    case "small":
+      styles += "p-2";
+      break;
+    case "medium":
+      styles += "p-4";
+      break;
+    case "large":
+      styles += "p-6";
+      break;
+
+    default:
+      styles += "p-2";
+      break;
+  }
+  return <button className={styles}>{icon}</button>;
 };
 
 export default IconButton;
